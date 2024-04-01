eb22fd45c0c6705c216f58fed3a6e0628af70bda
Evan Earle <evan.earle@gmail.com>
2024-04-01 16:43:00 -0300

    further styled ticket card

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -17,8 +17,16 @@ const TicketCard = () => {
       <p className="whitespace-pre-wrap">
         This is the ticket description. Please do this ticket.
       </p>
-      <ProgressDisplay />
-      <StatusDisplay />
+      <div className="flex-grow"></div>
+      <div className="flex mt-2 ">
+        <div className="flex flex-col ">
+          <p className="text-xs my-1">04/10/24 9:20PM</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
